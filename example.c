ab4d8a2c2c526cceb565c0f48abaf2f1f92a343b
StevenSYS <139715581+StevenSYS@users.noreply.github.com>
2026-05-04 22:03:03 +0000

    Added resizing to the example file.

--- example.c
+++ example.c
@@ -3,18 +3,26 @@
 
 #include "voArray.h"
 
-VOARRAY(intDA_t, int);
+VOARRAY(int_vo, int);
 
 int main() {
 	VOARRAY_TYPE_SIZE i;
-	intDA_t intArray;
+	int_vo intArray;
 	
 	intArray.size = 3;
 	VOARRAY_INIT(int, intArray, 1);
 	
+	loop:
 	for (i = 0; i < intArray.size; i++) {
 		intArray.i[i] = (i + 1) * 5;
 		printf("%i\n", intArray.i[i]);
+	}
+	
+	if (intArray.size == 3) {
+		intArray.size++;
+		VOARRAY_RESIZE(int, intArray, 1);
+		printf("- Resized\n");
+		goto loop;
 	}
 	
 	VOARRAY_UNINIT(intArray);
